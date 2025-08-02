abb2eb6922dbbcc0dd9c6139ce4f86744c039f6a
Sebastian Agudelo <agudelojoan28@gmail.com>
2025-08-01 20:27:30 -0500

    starting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
+import { ProductosComponent } from './pages/productos/productos.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    NavComponent,
+    RouterLink
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Invervisa';
+  
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { EntradaComponent } from './pages/entrada/entrada.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'productos', component: ProductosComponent },
+    { path: 'entrada', component: EntradaComponent }
+];
